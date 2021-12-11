0bb8d3dbb439e09a6a093a8a57f08f1e39c04392
treytose <treyholthe@gmail.com>
2021-12-11 08:13:41 +0000

    added support for gunicorn

--- flask_gen_config.py
+++ flask_gen_config.py
@@ -1,6 +1,6 @@
 import os
 class FlaskGenConfig:
-    APP_PATH = "/home/ec2-user/projects/flask_gen/TestApp"
+    APP_PATH = "/home/ec2-user/projects/trisoft"
     DB_HOST = os.getenv("DB_HOST")
     DB_NAME = os.getenv("DB_NAME")
     DB_USER = os.getenv("DB_USER")
--- template/base_app/manage.py
+++ template/base_app/manage.py
@@ -2,11 +2,10 @@
 import argparse
 from app import create_app
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", help="Enter the name of the config object to use", default="default")
+args, unknown = parser.parse_known_args() 
+app = create_app(args.config)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", help="Enter the name of the config object to use", default="default")
-    args = parser.parse_args()    
-
-    app = create_app(args.config)
     app.run(host=app.config.get('HOST'), port=app.config.get('PORT'), debug=app.config.get('DEBUG'))
